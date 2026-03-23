cfea18f0b482d6de9e808c1d228c667ef09eefcd
Mike Robinson <mike.robinson1979@gmail.com>
2026-03-23 04:59:23 -0700

    major changes to code

--- js/copyright.js
+++ js/copyright.js
@@ -1,14 +1,5 @@
-window.addEventListener("DOMContentLoaded", function () {
-
-    const target = document.getElementById("copyright");
-
-    if (!target) return;
-
-    fetch("fragments/_copyright.html")
-        .then(response => response.text())
-        .then(data => {
-            target.innerHTML = data;
-        })
-        .catch(error => console.log("Error loading copyright:", error));
-
+document.addEventListener("DOMContentLoaded", function () {
+  const year = new Date().getFullYear();
+  document.getElementById("copyright").innerHTML =
+    "&copy; " + year + " Mike Robinson. All rights reserved.";
 });
